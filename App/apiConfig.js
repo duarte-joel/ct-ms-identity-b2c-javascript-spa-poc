807f168874c25d8fd5cb37855c8b23df967f7385
Joel Duarte <joel.duarte@carlsberg.com>
2021-05-26 09:52:11 +0100

    Adds api config.

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
-    b2cScopes: ["profile"],
-    webApi: "https://fabrikamb2chello.azurewebsites.net/hello"
+    b2cScopes: ["https://cbconsumertech.onmicrosoft.com/86e7cdd7-2495-4fa1-8295-80e6ca46fcdc/poc.write"],
+    webApi: "http://localhost:8080/hello"
   };
